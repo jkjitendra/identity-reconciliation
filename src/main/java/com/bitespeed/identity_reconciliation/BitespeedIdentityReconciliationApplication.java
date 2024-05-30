23dbc1ff4047bc4f47698cca8455c3bce44aee55
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-05-31 00:48:04 +0530

    initial setup

---
+++ src/main/java/com/bitespeed/identity_reconciliation/BitespeedIdentityReconciliationApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.identity_reconciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedIdentityReconciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedIdentityReconciliationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitespeed/identity_reconciliation/BitespeedIdentityReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed.identity_reconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedIdentityReconciliationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
